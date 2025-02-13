02e78526dc11fc1b97f8f858b73ee7cbd0e165b2
Alex Koen <git@koen.ca>
2025-02-12 19:28:51 -0800

    feat: mesh processing refactor

--- foundation/scrollcase/src/scrollcase/mesh.py
+++ foundation/scrollcase/src/scrollcase/mesh.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
 from enum import Enum, auto
+from functools import partial
 import logging
+from os import PathLike
 import tempfile
-from typing import Optional
+from typing import Callable, Optional
 
 import meshlib.mrmeshpy as mm
 import meshlib.mrmeshnumpy as mn
@@ -37,7 +40,7 @@ def _copy_meshlib(mesh: mm.Mesh):
     return copy
 
 
-def load_mesh(mesh_file: str) -> mm.Mesh:
+def load_mesh(mesh_file: PathLike) -> mm.Mesh:
     return mm.loadMesh(mesh_file)
 
 
@@ -64,27 +67,114 @@ def affine_rotation(angle_rad: float) -> mm.AffineXf3f:
     return rotation_mat_mm
 
 
-def build_lining(
-    mesh_scroll: mm.Mesh,
-    *,
-    rotation_strategy: RotationStrategy = RotationStrategy.THIRD_PRINCIPAL,
-    cylinder_axis_tol: float = 0.01,
-    voxel_size_diagonal_percent: float = 0.4,
-    simplify_max_error_diagonal_percent: float = 1,
-    target_scale_diagonal_mm: Optional[float] = None,
-    _debug=False,
-) -> tuple[mm.Mesh, mm.Mesh, mm.Mesh, mm.Mesh, mm.Mesh, float, float]:
-    """Build lining given a scroll mesh.
+def mesh_smooth_denoise(mesh: mm.Mesh, gamma: float = 20) -> mm.Mesh:
+    """Smooth a mesh with a denoise operation.
+
+    Denoises a mesh per the paper 'Mesh Denoising via a Novel Mumford-Shah Framework'.
 
     Args:
-        mesh_scroll: Scroll mesh. See load_mesh.
-        rotation_strategy: How to rotate the mesh with respect to the cylinder axis.
-        Defaults to RotationStrategy.DEG_45.
+        gamma: Smoothing amount.
+    """
+    logger.info(f"Smoothing mesh with denoise, gamma = {gamma}")
+    mesh_smooth = _copy_meshlib(mesh)
+    settings = mm.DenoiseViaNormalsSettings()
+    settings.gamma = gamma
+    mm.meshDenoiseViaNormals(mesh_smooth)
+    return mesh_smooth
+
+
+def mesh_smooth_shrink_expand(
+    mesh: mm.Mesh, voxel_size: float, amount_mm: float = 2, shrink_first: bool = True
+):
+    """Smooth a mesh with a shrink/expand operation.
+
+    Shrinking first smoothes convexities, expanding first smoothes concavities.
+    """
+    mesh_smooth = _copy_meshlib(mesh)
+    logger.info(f"Smoothing mesh with a shrink/expand offset of {amount_mm} mm")
+    params = mm.OffsetParameters()
+    params.voxelSize = voxel_size
+    mesh_smooth = mm.offsetMesh(
+        mesh_smooth,
+        offset=-amount_mm if shrink_first else amount_mm,
+        params=params,  # type: ignore
+    )
+    mesh_smooth = mm.offsetMesh(
+        mesh_smooth,
+        offset=amount_mm if shrink_first else -amount_mm,
+        params=params,  # type: ignore
+    )
+    return mesh_smooth
+
+
+def get_principal_components(mesh: mm.Mesh) -> np.ndarray:
+    """Get principal components of mesh as a 3x3 array."""
+    vertices = mn.getNumpyVerts(mesh)
+    centroid = np.mean(vertices, axis=0)
+    centered_vertices = vertices - centroid
+    cov_matrix = np.cov(centered_vertices, rowvar=False)
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    principal_components = eigenvectors[:, sorted_indices].T
+    return principal_components
+
+
+def rotate_about_2nd_principal(mesh: mm.Mesh, rotation_rad: float) -> mm.Mesh:
+    """Rotate about second principal.
+
+    Rotate mesh so that 2nd principal component is rotatation_rad radians from the
+    second (wide) principal component.
+    """
+    principal_components = get_principal_components(mesh)
+    rotated_mesh = _copy_meshlib(mesh)
+
+    # NOTE(akoen): Technically we should project the principal component
+    first_rot = np.arccos(np.dot(principal_components[1], [1, 0, 0]))
+    rotated_mesh.transform(affine_rotation(first_rot + rotation_rad))
+    return rotated_mesh
+
+
+@dataclass
+class ScrollMesh:
+    """Scroll mesh processing options.
+
+    Attributes:
+        mesh_path: Path to scroll mesh.
+        rotation_from_2nd_principal_rad. Rotation from the second principal vector
+        about the cylinder axis. Defaults to 0.
         voxel_size_diagonal_percent: Size of voxels as a percentage of the diagonal
         size. Defaults to 0.4.
         simplify_max_error_diagonal_percent: Maximum simplification error as a
         percentage of the diagonal size. Defaults to 1.
         target_scale_diagonal_mm: Target diagonal size of the mesh. Defaults to None.
+        rotation_callback: Rotation callback that accepts a mesh and returns a mesh
+        smoothing_callback: Smoothing callback that accepts a mesh and returns a mesh
+        smoothin_unite_with_original: Unite with the original after smoothing to ensure
+        mesh does not get smaller.
+
+    """
+
+    mesh_path: PathLike
+    cylinder_axis_tol: float = 0.01
+    voxel_size_diagonal_percent: float = 0.04
+    simplify_max_error_diagonal_percent: float = 1
+    target_scale_diagonal_mm: Optional[float] = None
+    rotation_callback: Optional[Callable[[mm.Mesh], mm.Mesh]] = partial(
+        rotate_about_2nd_principal, rotation_rad=0
+    )
+    smoothing_callback: Optional[Callable[[mm.Mesh], mm.Mesh]] = None
+    smoothing_unite_with_original: bool = True
+
+
+def build_lining(
+    mesh_params: ScrollMesh,
+    *,
+    _debug=False,
+) -> tuple[mm.Mesh, mm.Mesh, mm.Mesh, mm.Mesh, mm.Mesh, float, float]:
+    """Build lining given a scroll mesh.
+
+    Args:
+        mesh_params: ScrollMesh parameters.
 
     Returns:
         Lining meshes for the positive and negative side, cavity meshes for the
@@ -95,9 +185,14 @@ def build_lining(
     # may someday cause trouble: tunnels, holes, etc.
     # Look at Meshlib GitHub "heal", "tunnel"
 
+    mesh_scroll = load_mesh(mesh_params.mesh_path)
+
     # Optionally scale mesh
-    if target_scale_diagonal_mm:
-        scale = target_scale_diagonal_mm / mesh_scroll.computeBoundingBox().diagonal()
+    if mesh_params.target_scale_diagonal_mm:
+        scale = (
+            mesh_params.target_scale_diagonal_mm
+            / mesh_scroll.computeBoundingBox().diagonal()
+        )
 
         logger.info("Scaling mesh by factor %.2f", scale)
         scale_mat = mm.AffineXf3f.linear(mm.Matrix3f.scale(scale))
@@ -106,62 +201,50 @@ def build_lining(
         logger.warning("Mesh is very small. Have you forgotten to scale?")
 
     voxel_size = (
-        mesh_scroll.computeBoundingBox().diagonal() * voxel_size_diagonal_percent / 100
+        mesh_scroll.computeBoundingBox().diagonal()
+        * mesh_params.voxel_size_diagonal_percent
+        / 100
     )
 
     logger.debug(f"Initial mesh: {count_vertices(mesh_scroll)} vertices")
 
     # Simplify mesh
     decimate_settings = mm.DecimateSettings()
-    decimate_settings.maxError = simplify_max_error_diagonal_percent / 100
+    decimate_settings.maxError = mesh_params.simplify_max_error_diagonal_percent / 100
     decimate_settings.packMesh = True
     mm.decimateMesh(mesh_scroll, decimate_settings)
 
     logger.debug(f"Simplified mesh: {count_vertices(mesh_scroll)} vertices")
 
+    # Fit minimum boundign cylinder
     logger.info("Aligning mesh")
     points = np.array([(point.x, point.y, point.z) for point in mesh_scroll.points])
     rotation_mat, translation_mat, radius, height = alignment.fit_cylinder(
-        points, cylinder_axis_tol, _debug=_debug
+        points, mesh_params.cylinder_axis_tol, _debug=_debug
     )
 
-    # Fit minimum bounding cylinder
     A = mm.Matrix3f(*[mm.Vector3f(*d) for d in rotation_mat])
     mesh_scroll.transform(mm.AffineXf3f(A, mm.Vector3f(*translation_mat)))
 
     # Optimize Rotation
-    vertices = mn.getNumpyVerts(mesh_scroll)
-    centroid = np.mean(vertices, axis=0)
-    centered_vertices = vertices - centroid
-    cov_matrix = np.cov(centered_vertices, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    principal_components = eigenvectors[:, sorted_indices].T
+    if mesh_params.rotation_callback is not None:
+        mesh_scroll = mesh_params.rotation_callback(mesh_scroll)
 
-    match rotation_strategy:
-        case RotationStrategy.NONE:
-            pass
-        case RotationStrategy.SECOND_PRINCIPAL:
-            # NOTE(akoen): Technically we should project the principal component
-            rotation = np.arccos(np.dot(principal_components[1], [1, 0, 0]))
-            mesh_scroll.transform(affine_rotation(rotation))
-
-        case RotationStrategy.THIRD_PRINCIPAL:
-            # NOTE(akoen): Technically we should project the principal component
-            rotation = np.arccos(np.dot(principal_components[2], [1, 0, 0]))
-            mesh_scroll.transform(affine_rotation(rotation))
-
-        case RotationStrategy.DEG_45:
-            # NOTE(akoen): Technically we should project the principal component
-            rotation = np.arccos(np.dot(principal_components[2], [1, 0, 0]))
-            rotation += np.pi / 4
-            mesh_scroll.transform(affine_rotation(rotation))
+    # Smooth mesh
+    if (sc := mesh_params.smoothing_callback) is not None:
+        mesh_scroll_smoothed = sc(mesh_scroll)
+        if mesh_params.smoothing_unite_with_original:
+            mesh_scroll = mm.voxelBooleanUnite(
+                mesh_scroll, mesh_scroll_smoothed, voxel_size
+            )
+        else:
+            mesh_scroll = mesh_scroll_smoothed
 
     # Offset 2mm
     logger.info("Offsetting mesh")
     params = mm.OffsetParameters()
     params.voxelSize = voxel_size
-    mesh_offset_2mm = mm.offsetMesh(mesh_scroll, offset=2, params=params)
+    mesh_offset_2mm = mm.offsetMesh(mesh_scroll, offset=2, params=params)  # type: ignore
 
     logger.debug(f"Offset mesh: {count_vertices(mesh_offset_2mm)} vertices")
 
@@ -188,9 +271,10 @@ def build_lining(
 
     logger.debug(f"Cavity_mesh_pos: {count_vertices(cavity_mesh_pos)} vertices")
 
+    # Build mesh
     logger.info("Building lining")
-    cavity_mesh_pos_offset = mm.offsetMesh(cavity_mesh_pos, offset=2, params=params)
-    cavity_mesh_neg_offset = mm.offsetMesh(cavity_mesh_neg, offset=2, params=params)
+    cavity_mesh_pos_offset = mm.offsetMesh(cavity_mesh_pos, offset=2, params=params)  # type: ignore
+    cavity_mesh_neg_offset = mm.offsetMesh(cavity_mesh_neg, offset=2, params=params)  # type: ignore
     mm.trimWithPlane(cavity_mesh_pos_offset, cut_plane, outCutEdges=hole_edges_pos)
     mm.trimWithPlane(cavity_mesh_neg_offset, -cut_plane, outCutEdges=hole_edges_neg)
 
