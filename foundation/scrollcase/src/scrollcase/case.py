1e6d6a050df9e814b18a682c4c146fce96d84b7a
Stephen Parsons <stephen@scrollprize.org>
2025-02-21 12:22:24 -0500

    Adjust alignment ring spacing

--- foundation/scrollcase/src/scrollcase/case.py
+++ foundation/scrollcase/src/scrollcase/case.py
@@ -44,7 +44,7 @@ class ScrollCase:
     mount_disc_box_width_mm: float = 13.5
 
     # Alignment ring spacing (up from bottom of lining interior)
-    alignment_ring_spacing_mm: Optional[float] = 100
+    alignment_ring_spacing_mm: Optional[float] = 98
     alignment_ring_width_mm: Optional[float] = 1.5
 
     # Alignment nubs
